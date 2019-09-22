c47d8f2989b010d2783732ab35bf4b469cbe5398
Claudio d'Angelis <claudiodangelis@gmail.com>
2019-09-22 19:33:14 +0200

    started working on documents

--- module/documents/documents.go
+++ module/documents/documents.go
@@ -1,5 +1,20 @@
 package documents
 
+import (
+	"os"
+	"time"
+)
+
+// Document represents a document
+type Document struct {
+	// TODO: or maybe "Title"?
+	Name      string
+	Directory string
+	MimeType  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
 // Module module
 type Module struct{}
 
@@ -10,6 +25,10 @@ func (b Module) Name() string {
 
 // Init initializes the module
 func (b Module) Init() error {
+	// Create "documents" directory
+	if err := os.Mkdir("documents", os.ModePerm); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -21,4 +40,10 @@ func (b Module) Check() error {
 // New module
 func New() Module {
 	return Module{}
+}
+
+// list documents
+func list() ([]Document, error) {
+	documents := []Document{}
+	return documents, nil
 }
--- module/documents/documents_test.go
+++ module/documents/documents_test.go
@@ -1,1 +1,28 @@
 package documents
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_list(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    []Document
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := list()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("list() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("list() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
